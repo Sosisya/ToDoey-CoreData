0809ed9dedfeb5265902418b911b5f1fe77ed70f
Sosisya <khaj-luizkin@yandex.ru>
2023-03-24 08:27:17 +0300

    Add FileManager

--- ToDoey/Controller/ToDoListTableViewController.swift
+++ ToDoey/Controller/ToDoListTableViewController.swift
@@ -21,6 +21,10 @@ class ToDoListTableViewController: UITableViewController {
         var item = Item()
         item.title = "Find Mike"
         itemArray.append(item)
+
+        saveItems()
+        loadItems()
+
     }
 
     // MARK: - Table view data source
@@ -32,7 +36,14 @@ class ToDoListTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemTableViewCell", for: indexPath) as! ToDoItemTableViewCell
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     private func configureTableView() {
@@ -40,20 +51,52 @@ class ToDoListTableViewController: UITableViewController {
     }
 
     private func cofigureNavigationBar() {
+        title = "ToDoey"
         let addBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .done, target: self, action: #selector(clickButton))
         addBarButtonItem.tintColor = .black
         self.navigationItem.rightBarButtonItem  = addBarButtonItem
     }
 
-    @objc private func clickButton(){
+    @objc private func clickButton() {
+        var textField = UITextField()
         let alert = UIAlertController(title: "Add item", message: "", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add", style: .default) {_ in
-            print("add")
+        let action = UIAlertAction(title: "Add", style: .default) { action  in
+            var newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            self.saveItems()
+
         }
-            alert.addAction(action)
-            alert.addTextField { textField in
-                textField.placeholder = "Add a task"
-            }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+
+        }
+        alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
+
+    private func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }  catch {
+            print("error: \(error)")
+        }
+        tableView.reloadData()
+    }
+
+    private func loadItems() {
+        if let data = try?  Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }  catch {
+                print("error: \(error)")
+            }
+        }
+        tableView.reloadData()
+    }
 }
+
